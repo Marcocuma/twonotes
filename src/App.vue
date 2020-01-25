acc919607326915f6257ca0205206fb0eca7bc4a
Marcocuma <46998207+Marcocuma@users.noreply.github.com>
2020-01-25 19:46:04 +0100

    Puedes ordenar notas y borrarlas

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
-  <div id="notas">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <input type="text" required v-model="nota">
-                <input type="button" v-on:click="add" value="Boton">
-                <input type="button" v-on:click="delCompletadas" value="Borrar Completadas">
-                <select v-model="prio">
-                    <option value="-1" >Baja</option>
-                    <option value="0">Media</option>
-                    <option value="1">Alta</option>
-                </select>
-            </div>
-        </nav>
-        <p>Tienes un total de {{ totalNotas }}</p>
-        <p>Completadas: {{ notasCompletadas }}</p>
-        <ol>
-            <nota @prioridad-cambiada="forzarUpdate()" v-for="(todo,index) in ordenarNotas" :key='index' :todo='todo'></nota>
+  <div id="notas" class="row">
+        <div id="barraBusqueda" class="col-12 d-flex">
+            <input type="text" class="w-100 m-4" required v-on:keypress="add" v-model="textoNota">
+        </div>
+        <div class="col-12 d-flex justify-content-around">
+            <p class="mb-0"><a v-on:click="compTodas" href="#">Completar todas </a>| Tienes un total de {{ totalNotas }} | Completadas: {{ notasCompletadas }} <a v-on:click="delCompletadas" href="#">Borrar Completadas</a></p>
+            <select v-model="orden">
+                <option value="1">Alfabetico</option>
+                <option value="2">Completadas</option>
+                <option value="3">Incompletas</option>
+                <option value="4">Prioridad</option>
+            </select>
+        </div>
+        <ol class="col-12">
+            <nota @prioridad-cambiada="forzarUpdate" @borrarNota="borrarNota" v-for="(todo,index) in ordenarNotas" :key='index' :index='index' :todo='todo'></nota>
         </ol>
     </div>
 </template>
@@ -37,6 +31,9 @@ export default {
         notas: [
         ],
         prio:'-1',
+        textoNota:'',
+        orden:"1",
+        completadas:false,
       }
   },
   mounted() {
@@ -49,8 +46,12 @@ export default {
   },
   methods:
     {
-        add: function(){
-            this.notas.push({text:this.nota,marcada:false,tiempo:Date.now(),prioridad:this.prio});
+        add: function(event){
+            if(event.keyCode == 13){
+                this.notas.push({text:this.textoNota,marcada:false,tiempo:Date.now(),prioridad:-1});
+                this.completadas=false;
+                this.textoNota=''
+            }
         },
         delCompletadas: function(){
             this.notas=this.notas.filter(function(elemento){
@@ -63,6 +64,22 @@ export default {
         },
         forzarUpdate: function(){
           this.$forceUpdate();
+        },
+        compTodas: function(){
+            if(!this.completadas){
+                this.notas.forEach((nota)=>{
+                    nota.marcada=true;
+                });
+                this.completadas=true;
+            } else {
+                this.notas.forEach((nota)=>{
+                    nota.marcada=false;
+                });
+                this.completadas=false;
+            }
+        },
+        borrarNota: function(todo){
+                this.notas.splice(this.notas.indexOf(todo),1);
         }
     },
     computed:
@@ -77,6 +94,24 @@ export default {
             return filtradas.length;
         },
         ordenarNotas: function(){
+            if(this.orden=="2")
+                return this.notas.slice().sort((nota1,nota2)=>{
+                    var a=nota1.marcada
+                    var b=nota2.marcada
+                    return a == false ? 1 : a==b ? 0 : -1
+                });
+            else if(this.orden=="3")
+                return this.notas.slice().sort((nota1,nota2)=>{
+                    var a=nota1.marcada
+                    var b=nota2.marcada
+                    return a == true ? 1 : a==b ? 0 : -1
+                });
+            else if(this.orden=="4")
+                return this.notas.slice().sort((nota1,nota2)=>{
+                    var a=nota1.prioridad
+                    var b=nota2.prioridad
+                    return a < b ? 1 : a == b ? 0 : -1
+                });
             return this.notas.slice().sort((nota1,nota2)=>{
                 var a=nota1.text.toLowerCase()
                 var b=nota2.text.toLowerCase()
@@ -95,5 +130,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ol{
+    list-style-type: none;
+    padding:0
 }
 </style>
--- src/components/nota.vue
+++ src/components/nota.vue
@@ -1,21 +1,27 @@
 <template lang="html">
 
-  <li>
-        <input type="checkbox" id="checkbox" v-model='todo.marcada'>
-        <label for="checkbox" v-bind:class='{terminada : todo.marcada}'> {{ todo.text }} </label>
-        <div id="tiempo"><span>La nota fue añadida hace {{ calculaTiempo(todo.tiempo) }} minutos</span></div>
-        <div class="btn-group btn-group-toggle" >
-            <label class="btn btn-outline-success " v-bind:class="[ todo.prioridad == -1 ? 'active' : '']">
-                <input type="radio" @click="cambiarPrioridad(todo,-1)" id="option1">Baja
-            </label>
-            <label class="btn btn-outline-primary" v-bind:class="[ todo.prioridad == 0 ? 'active' : '']">
-                <input type="radio" @click="cambiarPrioridad(todo,0)" id="option2">Media
-            </label>
-            <label class="btn btn-outline-danger" v-bind:class="[ todo.prioridad == 1 ? 'active' : '']">
-                <input type="radio" @click="cambiarPrioridad(todo,1)" id="option3">Alta
-            </label>
-        </div>
-        {{ todo.prioridad }}
+  <li class="m-4">
+      <div class="card">
+          <div class="card-body row d-flex align-items-center">
+            <input type="checkbox" class="col-1" v-model='todo.marcada'>
+            <p class="col-9" v-bind:class='{terminada : todo.marcada}'> {{ todo.text }} </p>
+            <img src="../assets/trash.png" @click="eliminarNota(todo)"/>
+          </div>
+          <div class="card-footer">
+            <div id="tiempo"><span>La nota fue añadida hace {{ calculaTiempo(todo.tiempo) }} minutos</span></div>
+            <div class="btn-group btn-group-toggle" >
+                <label class="btn btn-outline-success " v-bind:class="[ todo.prioridad == -1 ? 'active' : '']">
+                    <input type="radio" @click="cambiarPrioridad(todo,-1)" id="option1">Baja
+                </label>
+                <label class="btn btn-outline-primary" v-bind:class="[ todo.prioridad == 0 ? 'active' : '']">
+                    <input type="radio" @click="cambiarPrioridad(todo,0)" id="option2">Media
+                </label>
+                <label class="btn btn-outline-danger" v-bind:class="[ todo.prioridad == 1 ? 'active' : '']">
+                    <input type="radio" @click="cambiarPrioridad(todo,1)" id="option3">Alta
+                </label>
+            </div>
+          </div>
+      </div>
     </li>
 
 </template>
@@ -24,7 +30,7 @@
 
   export default  {
     name: 'nota',
-    props: ['todo'],
+    props: ['todo','key',],
     mounted () {
 
     },
@@ -41,6 +47,9 @@
         cambiarPrioridad: function(todo,number){
           todo.prioridad=number
           this.$emit('prioridad-cambiada');
+        },
+        eliminarNota: function(key){
+          this.$emit('borrarNota',key);
         }
     },
     computed: {
@@ -54,5 +63,14 @@
 <style scoped lang="css">
   .nota {
 
+  }
+  .card-body img{
+    height:3em;
+    width:3em;
+  }
+  .card-body img:hover{
+    background-color: rgba(128, 46, 46, 0.507);
+    border-radius: 20%;
+    transition:1s
   }
 </style>
